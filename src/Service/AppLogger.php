de263314ae8e58a6b90da58962aef0313a8c126f
akrick <akrick@sina.com>
2022-05-27 17:41:53 +0800

    update test

--- src/Service/AppLogger.php
+++ src/Service/AppLogger.php
@@ -1,10 +1,11 @@
 <?php
 
 namespace App\Service;
-
+use think\facade\Log as ThinkLog;
 class AppLogger
 {
     const TYPE_LOG4PHP = 'log4php';
+    const TYPE_THINKLOG = 'think-log';
 
     private $logger;
 
@@ -12,6 +13,18 @@ class AppLogger
     {
         if ($type == self::TYPE_LOG4PHP) {
             $this->logger = \Logger::getLogger("Log");
+        }elseif ($type == self::TYPE_THINKLOG){
+            $options = [
+                'default'	=>	'file',
+                'channels'	=>	[
+                    'file'	=>	[
+                        'type'	=>	'file',
+                        'path'	=>	'./logs/',
+                    ],
+                ],
+            ];
+
+            $this->logger = ThinkLog::instance($options);
         }
     }
 
--- tests/App/DemoTest.php
+++ tests/App/DemoTest.php
@@ -3,12 +3,15 @@
  * @Date         : 2022-03-02 14:49:25
  * @LastEditors  : Jack Zhou <jack@ks-it.co>
  * @LastEditTime : 2022-03-02 17:22:16
- * @Description  : 
+ * @Description  :
  * @FilePath     : /recruitment-php-code-test/tests/App/DemoTest.php
  */
 
 namespace Test\App;
 
+use App\App\Demo;
+use App\Service\AppLogger;
+use App\Util\HttpRequest;
 use PHPUnit\Framework\TestCase;
 
 
@@ -19,6 +22,10 @@ class DemoTest extends TestCase {
     }
 
     public function test_get_user_info() {
-
+        $logger = new AppLogger('think-log');
+        $request = new HttpRequest();
+        $demo = new Demo($logger, $request);
+        $json_data = $demo->get_user_info();
+        $this->assertJson($json_data);
     }
 }
