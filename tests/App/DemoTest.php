fe885de7a19a7d9bcaa39c4981b8d10a01b8c669
akrick <akrick@sina.com>
2022-05-30 16:09:00 +0800

    update test

--- tests/App/DemoTest.php
+++ tests/App/DemoTest.php
@@ -26,6 +26,14 @@ class DemoTest extends TestCase {
         $request = new HttpRequest();
         $demo = new Demo($logger, $request);
         $json_data = $demo->get_user_info();
-        $this->assertJson($json_data);
+        $result = array(
+            'error' => 0,
+            'data' => array(
+                'id' => 1,
+                "username" => "hello world"
+            ),
+        );
+        $this->expectOutputString(json_encode($result));
+        print $json_data;
     }
 }
